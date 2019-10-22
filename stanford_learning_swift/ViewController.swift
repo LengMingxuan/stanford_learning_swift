70ae798e974e37b5d381ac45a1f2bf60b6c8e57c
lengmingxuan <983835745@qq.com>
2019-10-23 00:04:45 +0800

    first

---
+++ stanford_learning_swift/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  stanford_learning_swift
+//
+//  Created by Mingxuan Leng on 2019/10/22.
+//  Copyright © 2019 Leng Mingxuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+}
+
